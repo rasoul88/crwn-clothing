0af3392940981dd4828929c47ff9081c8da0d5ce
rasoul88 <rasoulsahraei75@gmail.com>
2019-12-14 14:42:28 +0330

    Add particles background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
-import HomePage from './pages/homepage.component'
+import HomePage from './pages/homepage.component';
+
+const particlesOptions={
+  particles: {
+    number:{
+      value:30,
+      density:{
+        enable:true,
+        value_area:200
+      }
+    }
+  }
+}
 
 function App() {
   return (
-    <div >
+    <div className='App'>
+      <Particles className='particles' params={particlesOptions}/> 
       <HomePage />
     </div>
   );
