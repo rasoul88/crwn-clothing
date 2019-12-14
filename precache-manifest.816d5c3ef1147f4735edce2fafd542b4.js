4160436ac225d91b8c800fbc896687698180e290
rasoul88 <rasoulsahraei75@gmail.com>
2019-12-14 14:53:42 +0330

    Updates

---
+++ precache-manifest.816d5c3ef1147f4735edce2fafd542b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0444888bcc037c56acd57c709c34a06e",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "2fe2cf5bd70b13df795c",
+    "url": "/crwn-clothing/static/css/main.9b3274b0.chunk.css"
+  },
+  {
+    "revision": "39ac26a221b829901d38",
+    "url": "/crwn-clothing/static/js/2.09b810bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crwn-clothing/static/js/2.09b810bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fe2cf5bd70b13df795c",
+    "url": "/crwn-clothing/static/js/main.ee326d3f.chunk.js"
+  },
+  {
+    "revision": "8e1f379f0858b5b017bc",
+    "url": "/crwn-clothing/static/js/runtime-main.01128f07.js"
+  }
+]);
