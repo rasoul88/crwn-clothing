b41d62e50744225f7e6c97bacec15fbf41d94ae4
rasoul88 <rasoulsahraei75@gmail.com>
2020-02-11 22:36:43 +0330

    updating stripe integration

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -18,7 +18,7 @@ const CartDropdown = ({cartItems, history, dispatch}) => (
             }
         </div>
         <CustomButton onClick={() => {
-            history.push('./checkout');
+            history.push('/checkout');
             dispatch(toggleCartHidden());
             }} 
             CartDropdown>GO TO CHECKOUT</CustomButton>
